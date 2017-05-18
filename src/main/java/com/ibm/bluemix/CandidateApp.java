01459b83ea6fd6e7c722525b856e992340931401
omar-meshaal <omar.meshaal@gmail.com>
2017-05-18 20:11:10 +0200

    refactored personoutGenerator by using intorducing refactor technique

--- src/main/java/com/ibm/bluemix/CandidateApp.java
+++ src/main/java/com/ibm/bluemix/CandidateApp.java
@@ -13,9 +13,20 @@ public class CandidateApp {
 			personList.addAll(PersonLoader.loadPersonRecords("./data/pipe.txt", new String[] {Person.LAST_NAME, Person.FIRST_NAME, Person.IGNORE, Person.GENDER, Person.COLOR, Person.DATE_OF_BIRTH}, "\\s*\\|\\s*"));
 			personList.addAll(PersonLoader.loadPersonRecords("./data/comma.txt", new String[] {Person.LAST_NAME, Person.FIRST_NAME, Person.GENDER, Person.COLOR, Person.DATE_OF_BIRTH}, "\\s*,\\s*"));
 
-			PersonOutputGenerator.generateOutput1(personList);
-			PersonOutputGenerator.generateOutput2(personList);
-			PersonOutputGenerator.generateOutput3(personList);
+			PersonOutputGenerator.generateOutput(personList, (Person p1, Person p2) -> {
+            if(!p1.getGender().equals(p2.getGender())) {
+              return p1.getGender().compareTo(p2.getGender());
+            }
+            return p1.getLastName().compareTo(p2.getLastName());
+          }, "Output 1:\n");
+			PersonOutputGenerator.generateOutput(personList, (Person p1, Person p2) -> {
+            if(p1.getDateOfBirth().compareTo(p2.getDateOfBirth()) != 0) {
+              return p1.getDateOfBirth().compareTo(p2.getDateOfBirth());
+            }
+
+            return p1.getLastName().compareTo(p2.getLastName());
+          }, "\nOutput 2:\n");
+			PersonOutputGenerator.generateOutput(personList, (Person p1, Person p2) -> p2.getLastName().compareTo(p1.getLastName()), "\nOutput 3:\n");
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
--- src/main/java/com/ibm/bluemix/PersonOutputGenerator.java
+++ src/main/java/com/ibm/bluemix/PersonOutputGenerator.java
@@ -1,45 +1,13 @@
 package com.ibm.bluemix;
 
+import java.util.Comparator;
 import java.util.List;
 
 class PersonOutputGenerator {
-  static void generateOutput1(List<Person> personList) {
-    personList.sort((Person p1, Person p2) -> {
-        if(!p1.getGender().equals(p2.getGender())) {
-          return p1.getGender().compareTo(p2.getGender());
-        }
-        return p1.getLastName().compareTo(p2.getLastName());
-      }
-    );
+  static void generateOutput(List<Person> personList, Comparator<Person> comparator, String outputMsg) {
+    personList.sort(comparator);
 
-    System.out.println("Output 1:\n");
-
-    for(Person person : personList) {
-      System.out.println(person);
-    }
-  }
-
-  static void generateOutput2(List<Person> personList) {
-    personList.sort((Person p1, Person p2)-> {
-        if(p1.getDateOfBirth().compareTo(p2.getDateOfBirth()) != 0) {
-          return p1.getDateOfBirth().compareTo(p2.getDateOfBirth());
-        }
-
-        return p1.getLastName().compareTo(p2.getLastName());
-      }
-    );
-
-    System.out.println("\nOutput 2:\n");
-
-    for(Person person : personList) {
-      System.out.println(person);
-    }
-  }
-
-  static void generateOutput3(List<Person> personList) {
-    personList.sort((Person p1, Person p2) -> p2.getLastName().compareTo(p1.getLastName()));
-
-    System.out.println("\nOutput 3:\n");
+    System.out.println(outputMsg);
 
     for(Person person : personList) {
       System.out.println(person);
