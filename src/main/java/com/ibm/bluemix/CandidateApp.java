16c6708f586432aa3aa9fca673a592c75e365422
omar-meshaal <omar.meshaal@gmail.com>
2017-05-19 15:54:26 +0200

    add constant class for delimiters and formats

--- src/main/java/com/ibm/bluemix/CandidateApp.java
+++ src/main/java/com/ibm/bluemix/CandidateApp.java
@@ -5,31 +5,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CandidateApp {
-	public static void main(String[] args) {
-		List<Person> personList = new ArrayList<Person>();
-		
-		try {
-			personList.addAll(PersonLoader.loadPersonRecords("./data/space.txt", new String[] {Person.LAST_NAME, Person.FIRST_NAME, Person.IGNORE, Person.GENDER, Person.DATE_OF_BIRTH, Person.COLOR}, "\\s+"));
-			personList.addAll(PersonLoader.loadPersonRecords("./data/pipe.txt", new String[] {Person.LAST_NAME, Person.FIRST_NAME, Person.IGNORE, Person.GENDER, Person.COLOR, Person.DATE_OF_BIRTH}, "\\s*\\|\\s*"));
-			personList.addAll(PersonLoader.loadPersonRecords("./data/comma.txt", new String[] {Person.LAST_NAME, Person.FIRST_NAME, Person.GENDER, Person.COLOR, Person.DATE_OF_BIRTH}, "\\s*,\\s*"));
 
-			PersonOutputGenerator.generateOutput(personList, (Person p1, Person p2) -> {
-            if(!p1.getGender().equals(p2.getGender())) {
-              return p1.getGender().compareTo(p2.getGender());
-            }
-            return p1.getLastName().compareTo(p2.getLastName());
-          }, "Output 1:\n");
-			PersonOutputGenerator.generateOutput(personList, (Person p1, Person p2) -> {
-            if(p1.getDateOfBirth().compareTo(p2.getDateOfBirth()) != 0) {
-              return p1.getDateOfBirth().compareTo(p2.getDateOfBirth());
-            }
+  public static void main(String[] args) {
+    List<Person> personList = new ArrayList<Person>();
 
-            return p1.getLastName().compareTo(p2.getLastName());
-          }, "\nOutput 2:\n");
-			PersonOutputGenerator.generateOutput(personList, (Person p1, Person p2) -> p2.getLastName().compareTo(p1.getLastName()), "\nOutput 3:\n");
+    try {
+      loadPersons(personList);
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+      generateOutput(personList);
+
+    }
+    catch(FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void generateOutput(List<Person> personList) {
+    PersonOutputGenerator.generateOutput(personList, Sorter.genderThenLastName(), "Output 1:\n");
+    PersonOutputGenerator.generateOutput(personList, Sorter.dobThenLastName(), "\nOutput 2:\n");
+    PersonOutputGenerator.generateOutput(personList, Sorter.lastNameDescending(), "\nOutput 3:\n");
+  }
+
+  private static void loadPersons(List<Person> personList) throws FileNotFoundException {
+    personList.addAll(PersonLoader.loadPersonRecords("./data/space.txt", FileFormats.SPACE_FILE, Delimiters.SPACE));
+    personList.addAll(PersonLoader.loadPersonRecords("./data/pipe.txt", FileFormats.PIPE_FILE, Delimiters.PIPE));
+    personList.addAll(PersonLoader.loadPersonRecords("./data/comma.txt", FileFormats.COMMA_FILE, Delimiters.COMMA));
+  }
+
 }
